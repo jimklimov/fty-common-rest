2531202ea8a220bebfd93b4407c46e2d7f6ec22a
Jiri Kukacka <JiriKukacka@Eaton.com>
2019-07-30 09:31:38 +0000

    Macros shouldn't contain whitespaces in definition

--- include/fty_common_rest_audit_log.h
+++ include/fty_common_rest_audit_log.h
@@ -32,24 +32,24 @@
 
 
 /* Prints message in Audit Log with DEBUG level. */
-#define log_debug_audit (...) \
-        log_debug_log (AuditLogManager::getInstance (), __VA_ARGS__);
+#define log_debug_audit(...) \
+        log_debug_log(AuditLogManager::getInstance(), __VA_ARGS__);
 
 /* Prints message in Audit Log with INFO level. */
-#define log_info_audit (...) \
-        log_info_log (AuditLogManager::getInstance (), __VA_ARGS__);
+#define log_info_audit(...) \
+        log_info_log(AuditLogManager::getInstance(), __VA_ARGS__);
 
 /* Prints message in Audit Log with WARNING level*/
-#define log_warning_audit (...) \
-        log_warning_log (AuditLogManager::getInstance (), __VA_ARGS__);
+#define log_warning_audit(...) \
+        log_warning_log(AuditLogManager::getInstance(), __VA_ARGS__);
 
 /* Prints message in Audit Log with ERROR level*/
-#define log_error_audit (...) \
-        log_error_log (AuditLogManager::getInstance (), __VA_ARGS__);
+#define log_error_audit(...) \
+        log_error_log(AuditLogManager::getInstance(), __VA_ARGS__);
 
 /* Prints message in Audit Log with FATAL level. */
-#define log_fatal_audit (...) \
-        log_fatal_log (AuditLogManager::getInstance (), __VA_ARGS__);
+#define log_fatal_audit(...) \
+        log_fatal_log(AuditLogManager::getInstance(), __VA_ARGS__);
 
 //singleton for logger management
 class AuditLogManager
